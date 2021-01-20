5957de2a690d39569ccf302587d473ee3db631d3
kevin.claassen <claassenk@outlook.com>
2021-01-20 23:12:38 +0100

    Created ViewModel

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,20 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private val _status = MutableLiveData<String>()
+    val status: LiveData<String>
+        get() = _status
+
+    init {
+        getAstroidProperties()
+    }
+
+    private fun getAstroidProperties() {
+        _status.value = "Set the API response here"
+    }
 }
