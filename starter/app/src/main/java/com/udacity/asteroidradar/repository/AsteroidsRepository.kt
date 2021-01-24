dfa271dac958184a84773018115a64544bfbb256
kevin.claassen <claassenk@outlook.com>
2021-01-24 15:33:53 +0100

    Added Entities, Database, Domain and Dao

--- starter/app/src/main/java/com/udacity/asteroidradar/repository/AsteroidsRepository.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/repository/AsteroidsRepository.kt
@@ -1,8 +1,13 @@
 package com.udacity.asteroidradar.repository
 
+import android.net.Network
+import com.udacity.asteroidradar.Constants.API_KEY
+import com.udacity.asteroidradar.api.getSeventhDay
+import com.udacity.asteroidradar.api.getToday
 import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.network.AsteroidApi
+import com.udacity.asteroidradar.network.NetworkAsteroid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
@@ -12,10 +17,12 @@ class AsteroidsRepository(private val database: AsteroidDatabase) {
 
     var AsteroidApiService = AsteroidApi.retrofitService
 
+
     suspend fun refreshAstroids() {
         withContext(Dispatchers.IO) {
-            val jsonResult = AsteroidApiService.getAsteroids()
+            val jsonResult = AsteroidApiService.getAsteroids(getToday(), getSeventhDay(), API_KEY)
             val asteroids = parseAsteroidsJsonResult(JSONObject(jsonResult))
+            database.astreoidsDao.insertAll(*asteroids.asDatabaseModel())
         }
     }
 
