ffbe3fdefabff12ad79171de889f148a29bfedcb
kevin.claassen <claassenk@outlook.com>
2021-01-25 01:01:18 +0100

    Added databinding to inflate Fragment_Main layout within MainFragment.kt

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
@@ -9,12 +10,13 @@ import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    lateinit var binding: FragmentMainBinding
+
     /**
      * Lazily initialize our [AsteroidListViewModel].
      */
     private val viewModel: MainViewModel by lazy {
         val activity = requireNotNull(this.activity) {
-            "You can only access the viewModel after onViewCreated()"
         }
         ViewModelProvider(this, MainViewModel.Factory(activity.application)).get(
             MainViewModel::class.java
@@ -23,7 +25,8 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentMainBinding.inflate(inflater)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
--- starter/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWork.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWork.kt
@@ -10,6 +10,10 @@ import retrofit2.HttpException
 class RefreshDataWork(appContext: Context, params: WorkerParameters):
     CoroutineWorker(appContext, params) {
 
+    companion object {
+        const val WORK_NAME = "RefreshDataWork"
+    }
+
     override suspend fun doWork(): Result {
         val database =  getDatabase(applicationContext)
         val repository = AsteroidsRepository(database)
